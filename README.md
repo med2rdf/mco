9ae7d330f84d747d3903d1307a34bdc46bca4c6e
Toshiaki Katayama <k@bioruby.org>
2024-09-13 14:46:54 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# mco
-Mouse Chromosome Ontology
+# Mouse Chromosome Ontology
+
+The Mouse Chromosome Ontology (MCO) provides simple and stable URIs for mouse reference genome versions to semantically identify mouse chromosomes.
+
+Basic class `mco:MouseChromosome` is inherited by each chromosome class such as `mco:2` and `mco:GenomeBuild` is inherited by each build version such as `mco:GRCm39`. Then, for example, the mouse chromosome 2 of the GRCm39 version is represented as `mco:2\/GRCm39` (<http://identifiers.org/mco/2/GRCm39>) which is defined as an instance of `mco:2` with build version `mco:GRCm39`. Each instance of a chromosome has links to external resources such as INSDC, RefSeq, UCSC etc. so that users can be navigated to the chromosome sequence and its annotations.
+
+```
+mco:2
+        rdfs:label      "Mouse chromosome 2" ;
+        rdfs:subClassOf mco:MouseChromosome .
+
+mco:2\/GRCm39
+        rdf:type        mco:2 ;
+        mco:build       mco:GRCm39 ;
+        mco:length      46709983 ;
+        skos:altLabel   "GPC_000001313" ;
+        mco:insdc       <http://identifiers.org/insdc/CM000995.3> ;
+        mco:refseq      <http://identifiers.org/refseq/NC_000068.8> ;
+        mco:ucsc        <http://genome.ucsc.edu/cgi-bin/hgTracks?db=mm39&position=chr2> ;
+        mco:ensembl     <http://rdf.ebi.ac.uk/resource/ensembl/mus_musculus/GRCm39/2> ,
+                        <http://www.ensembl.org/Mus_musculus/Location/Chromosome?r=2> .
+```
+
+### Contributors
+
+* Toshiaki Katayama (Database Center for Life Science)
+* Toyoyuki Takada (RIKEN BioResource Research Center)
+* The Med2RDF project <http://med2rdf.org/>
+* VISC <https://github.com/dbcls/visc/>
+
---
+++ mco.rb
@@ -0,0 +1,64 @@
+#!/usr/bin/env ruby
+
+puts File.read("mco_head.ttl")
+
+mm39 = {}
+mm10 = {}
+
+def parse_chrom(chromfile, sizefile, hash)
+  File.open(chromfile).each do |line|
+    next if line[/^#/]
+    chr, assembly, ensembl, genbank, refseq = line.strip.split("\t")
+    next if chr[/_/]  
+    number = assembly[/(X|Y|MT)/] ? assembly : assembly.to_i
+    hash[number] = {
+      :chr => chr,
+      :ensembl => ensembl,
+      :genbank => genbank,
+      :refseq => refseq
+    }
+  end
+
+  File.open(sizefile).each do |line|
+    next if line[/^#/]
+    chr, len = line.strip.split("\t")
+    next if chr[/_/]
+    assembly = chr.sub(/^chr/, '').sub('M', 'MT')
+    number = assembly[/(X|Y|M)/] ? assembly : assembly.to_i
+    hash[number] ||= {}
+    hash[number][:length] = len.to_i  
+  end
+end
+  
+parse_chrom("mm39.chromAlias.txt", "mm39.chrom.sizes", mm39)
+parse_chrom("mm10.chromAlias.txt", "mm10.chrom.sizes", mm10)
+
+[(1..19).to_a, "X", "Y", "MT"].flatten.each do |num|
+  puts "mco:#{num}"
+  puts "\trdf:type\towl:Class ;"
+  puts "\trdfs:label\t\"Mouse chromosome 1\" ;"
+  puts "\trdfs:subClassOf\tmco:MouseChromosome ."
+  puts
+
+  puts "mco:#{num}\\/GRCm39"
+  puts "\trdf:type\tmco:#{num} ;"
+  puts "\tmco:build\tmco:GRCm39 ;"
+  puts "\tmco:length\t#{mm39[num][:length]} ;"
+  puts "\tmco:insdc\t<http://identifiers.org/insdc/#{mm39[num][:genbank]}> ;"
+  puts "\tmco:refseq\t<http://identifiers.org/refseq/#{mm39[num][:refseq]}> ;"
+  puts "\tmco:ucsc\t<http://genome.ucsc.edu/cgi-bin/hgTracks?db=mm39&position=#{mm39[num][:chr]}> ;"
+  puts "\tmco:ensembl\t<http://rdf.ebi.ac.uk/resource/ensembl/mus_musculus/GRCm39/#{num}> ,"
+  puts "\t\t<http://ensembl.org/Mus_musculus/Location/Chromosome?r=#{num}> ."
+  puts
+
+  puts "mco:#{num}\\/GRCm38"
+  puts "\trdf:type\tmco:#{num} ;"
+  puts "\tmco:build\tmco:GRCm38 ;"
+  puts "\tmco:length\t#{mm10[num][:length]} ;"
+  puts "\tmco:insdc\t<http://identifiers.org/insdc/#{mm10[num][:genbank]}> ;"
+  puts "\tmco:refseq\t<http://identifiers.org/refseq/#{mm10[num][:refseq]}> ;"
+  puts "\tmco:ucsc\t<http://genome.ucsc.edu/cgi-bin/hgTracks?db=mm39&position=#{mm10[num][:chr]}> ;"
+  puts "\tmco:ensembl\t<http://rdf.ebi.ac.uk/resource/ensembl/mus_musculus/GRCm38/#{num}> ,"
+  puts "\t\t<http://nov2020.archive.ensembl.org/Mus_musculus/Location/Chromosome?r=#{num}> ."
+end
+
